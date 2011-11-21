b0ea5bf60524e65aa5c0fbde4f0920d43d15beb9
Patrick Johnstone <pjohnstone87@gmail.com>
2011-11-20 21:10:47 -0600

    Testing

---
+++ DragonFruit/DragonFruit/DragonFruitAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DragonFruitAppDelegate.h
+//  DragonFruit
+//
+//  Created by Patrick Johnstone on 11-11-20.
+//  Copyright 2011 University of Manitoba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DragonFruitAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ DragonFruit/DragonFruit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DragonFruit
+//
+//  Created by Patrick Johnstone on 11-11-20.
+//  Copyright 2011 University of Manitoba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
