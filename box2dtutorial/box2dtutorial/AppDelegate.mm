e728bb291566f934dadab48fca18c77c3be6f581
Patrick Johnstone <pjohnstone87@gmail.com>
2011-12-10 21:00:14 -0600

    small changes to box2d tutorial

--- box2dtutorial/box2dtutorial/AppDelegate.mm
+++ box2dtutorial/box2dtutorial/AppDelegate.mm
@@ -3,7 +3,7 @@
 //  box2dtutorial
 //
 //  Created by Patrick Johnstone on 11-12-08.
-//  Copyright University of Manitoba 2011. All rights reserved.
+//  Copyright NovoTeam 2011. All rights reserved.
 //
 
 #import "cocos2d.h"
